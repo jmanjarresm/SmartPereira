22e0450bcad7b419bbd278143e06c021b9ec35a6
jmanjarresm <124469422+jmanjarresm@users.noreply.github.com>
2023-08-15 22:07:13 -0500

    Create smartconversation.py

---
+++ smartconversation.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, jsonify
+import requests
+import json
+import os
+
+app = Flask(__name__)
+app.static_folder = 'static'
+
+API_URL = "https://flowisetest-1.onrender.com/api/v1/prediction/dbaf12c5-c585-42e3-a057-ce4e41dbbd26"
+
+def query(payload):
+    response = requests.post(API_URL, json=payload)
+    try:
+        response_json = response.json()
+        return response_json
+    except json.JSONDecodeError:
+        return {"error": "Invalid response from API"}
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        user_input = request.form.get("user_input")
+        if user_input.strip() != "":
+            response = query({"question": user_input})
+            bot_response = response.get("text")
+            return render_template("index.html", user_input=user_input, bot_response=bot_response)
+    return render_template("index.html", user_input="", bot_response="")
+
+@app.route("/get_response", methods=["POST"])
+def get_response():
+    user_input = request.form.get("user_input")
+    if user_input.strip() != "":
+        response = query({"question": user_input})
+        bot_response = response.get("text")
+        return jsonify({"bot_response": bot_response})
+    return jsonify({"bot_response": ""})
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
